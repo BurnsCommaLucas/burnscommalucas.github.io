f70c64653d17f59a0f4d3958d5169ababc6cda05
Lucas Burns <lucasburns@lithia.com>
2020-06-07 14:59:08 -0700

    Almost got the http interceptor there

--- src/app/http-interceptors/ensure-https-interceptor.ts
+++ src/app/http-interceptors/ensure-https-interceptor.ts
@@ -8,7 +8,7 @@ export class EnsureHttpInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // clone request and replace 'http://' with 'https://' at the same time
         const secureReq = req.clone({
-        url: req.url.replace('http://', 'https://')
+            url: req.url.substr(0,4) === 'http' ? req.url.replace('http://', 'https://') : 'http://' + req.url
         });
         // send the cloned, "secure" request to the next handler.
         return next.handle(secureReq);

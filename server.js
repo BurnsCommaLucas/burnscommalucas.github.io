3902fb5ef57cfd7fac87cf5fd8094a29f9b6fef5
Lucas Burns <lucasburns@lithia.com>
2020-06-12 16:00:36 -0700

    Trying different approach for ssl

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // install express server
 const express = require('express');
 const path = require('path');
+const secure = require('ssl-express-www');
 const app = express();
-const forceSsl = require('force-ssl-heroku');
 
 //serve only static files from dist directory, require ssl
-app.use(express.static(__dirname + '/dist/personal-site'), forceSsl);
+app.use(express.static(__dirname + '/dist/personal-site'));
+app.use(secure);
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname+'/dist/personal-site/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('Listening on port %s', port));
